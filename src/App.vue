c8849802bbc009b51a425f4e13f78aa97805537b
ztavruz <ztavruz1987@mail.ru>
2019-05-18 11:43:26 +0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app">
+    <app-header></app-header>
+    <router-view/>
+  </div>
+</template>
+
+<script>
+import AppHeader from '@/components/AppHeader'
+
+export default {
+  components: {
+    AppHeader
+  }
+
+
+}
+</script>
+
+<style >
+
+</style>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,20 @@
+<template>
+    <v-app>
+        <v-toolbar app class="v-toolbar-custom">
+            <v-toolbar-title><a href=""><img src="../assets/logo.jpg" alt=""></a></v-toolbar-title>
+        </v-toolbar>
+    </v-app>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style scoped>
+.v-toolbar-custom{
+    background: white;
+}
+
+</style>
